f115f84ada680d89f18ab802e68a1c11be522a98
Jose Gonzalez <gonza.backup8@gmail.com>
2024-09-06 14:16:46 -0600

    verificacion inicial de RFC terminada

--- src/app/components/validar-rfc/validar-rfc.component.ts
+++ src/app/components/validar-rfc/validar-rfc.component.ts
@@ -19,4 +19,23 @@ export class ValidarRFCComponent {
     this.messageService.add({ severity: 'success', summary: 'Éxito', detail: mensaje });
   }
 
+  private validarFormatoRFC(rfc: string): boolean {
+    const regex = /^[A-Z]{4}\d{6}[0-9A-Z]{3}$/;
+    return regex.test(rfc);
+  }
+
+  validarRFC() {
+    if (this.rfc.length !== 13) {
+      this.mostrarError('El RFC debe tener exactamente 13 caracteres.');
+      return;
+    }
+
+    if (!this.validarFormatoRFC(this.rfc)) {
+      this.mostrarError('El formato del RFC no es válido.');
+      return;
+    }
+
+    this.mostrarExito('RFC válido.');
+  }
+
 }
